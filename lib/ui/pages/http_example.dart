b953f419af489867aa0002b5bb0c706664f1dedb
Hovikkk <minas.93@mail.ru>
2020-10-22 21:28:21 +0400

    get data json api

--- lib/ui/pages/http_example.dart
+++ lib/ui/pages/http_example.dart
@@ -4,65 +4,62 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Post> fetchPost() async {
-  final response =
-      await http.get("https://jsonplaceholder.typicode.com/posts/1");
-
-  if (response.statusCode == 200) {
-    return Post.fromJson(json.decode(response.body));
-  } else {
-    throw Exception("Failed to load the post, try again later");
-  }
-}
-
-class Post {
-  final int userID;
-  final int id;
-  final String title;
-  final String body;
-
-  Post({this.userID, this.id, this.title, this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-        userID: json['userId'],
-        id: json['id'],
-        title: json['title'],
-        body: json['body']);
-  }
-}
-
 class HttpPage extends StatefulWidget {
   @override
   _HttpPageState createState() => _HttpPageState();
 }
 
 class _HttpPageState extends State<HttpPage> {
-  final Future<Post> post = fetchPost();
+  List data;
+
+  Future<String> getData() async {
+    http.Response response = await http.get(
+      Uri.encodeFull("https://jsonplaceholder.typicode.com/posts/"),
+      headers: {"Accept": "Application/json"},
+    );
+
+    setState(() {
+      data = json.decode(response.body);
+    });
+
+    return "success";
+  }
+
+  void initState() {
+    this.getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Http'),
+        title: Center(child: Text("Json API")),
+        backgroundColor: Colors.green,
+        actions: [
+          SizedBox(
+            width: 50,
+            height: 50,
+          )
+        ],
       ),
       body: body(),
     );
   }
 
   Widget body() {
-    return Center(
-      child: FutureBuilder<Post>(
-        future: post,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Text(snapshot.data.title);
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
-
-          return CircularProgressIndicator();
-        },
-      ),
+    return ListView.builder(
+      itemCount: data == null ? 0 : data.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('${data[index]["id"]}. '),
+              Expanded(child: Text(data[index]["body"])),
+            ],
+          ),
+        );
+      },
     );
   }
 }
